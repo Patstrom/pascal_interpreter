9300ba0056e408813b8250abead8648f3fb014ec
Patrik Karlström <jp.karlstrom@gmail.com>
2017-10-02 01:14:50 +0200

    Add SymbolTable and SymbolTableBuilder

---
+++ symbol_table.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <string>
+#include <map>
+#include <memory>
+
+using namespace std;
+
+class Symbol {
+    protected:
+        string name;
+        std::shared_ptr<Symbol> type;
+    public:
+        Symbol() : name(""), type(nullptr) {}
+        Symbol(string n) : name(n), type(nullptr) {}
+        Symbol(string n, Symbol t) : name(n){
+            type = std::make_shared<Symbol>(t);
+        }
+
+        string get_name() { return name; }
+        std::shared_ptr<Symbol> get_type() { return type; }
+};
+
+class BuiltinType : public Symbol {
+    public:
+        BuiltinType(string n) : Symbol(n) {}
+};
+
+class VarSymbol : public Symbol {
+    public:
+        VarSymbol(string n, Symbol t) : Symbol(n, t) {}
+};
+
+class SymbolTable {
+    private:
+        map<string, Symbol> symbols;
+
+        void init_builtins() {
+            this->define(BuiltinType("INTEGER"));
+            this->define(BuiltinType("REAL"));
+        }
+    public:
+        SymbolTable() {
+            init_builtins();
+        }
+
+        void define(Symbol s) {
+            symbols[s.get_name()] = s;
+        }
+
+        Symbol lookup(string name) { return symbols[name]; }
+
+        Symbol& operator[](std::string idx) { return this->symbols[idx]; }
+
+        std::ostream& operator<<(std::ostream& strm) {
+            strm << "symbol_table: {" << std::endl;
+            for(auto elem : symbols) {
+                strm << "\t" << elem.first << " : " << elem.second.get_name() << std::endl;
+            }
+            return strm << "}";
+        }
+};
---
+++ symbol_table_builder.hpp
@@ -0,0 +1,112 @@
+#pragma once
+#include "symbol_table.hpp"
+#include <memory>
+
+typedef std::shared_ptr<Node> node_ptr;
+
+ class SymbolTableBuilder {
+    private:
+        SymbolTable symbols;
+    protected:
+        void visit_unary(node_ptr n) {
+            visit(n->get_left());
+        }
+
+        void visit_num(node_ptr n) {
+            return;
+        }
+
+        void visit_binop(node_ptr n) {
+            visit(n->get_left());
+            visit(n->get_right());
+        }
+
+        void visit_compound(node_ptr n) {
+            for(auto ptr: n->get_children()) {
+                visit(ptr);
+            }
+        }
+
+        void visit_noop(node_ptr n) {
+            return;
+        }
+
+        void visit_program(node_ptr n) {
+            visit(n->get_left());
+        }
+
+        void visit_block(node_ptr n) {
+            for(auto ptr: n->get_children()) {
+                visit(ptr);
+            }
+        }
+
+        void visit_vardecl(node_ptr n) {
+            string type_name = n->get_right()->get_token().get_value();
+            Symbol type_symbol = symbols.lookup(type_name);
+            string var_name = n->get_left()->get_token().get_value();
+            VarSymbol var_symbol(var_name, type_symbol);
+            symbols.define(var_symbol);
+        }
+
+        void visit_assign(node_ptr n) {
+            string var_name = n->get_left()->get_token().get_value();
+
+            Symbol var_symbol = symbols.lookup(var_name);
+            if ( var_symbol.get_name() == "" && var_symbol.get_type() == nullptr ) {
+                cout << "Variable is not defined" << endl;
+                exit(1);
+            }
+        }
+
+        void visit_var(node_ptr n) {
+            string var_name = n->get_token().get_value();
+
+            Symbol var_symbol = symbols.lookup(var_name);
+            if ( var_symbol.get_name() == "" && var_symbol.get_type() == nullptr ) {
+                cout << "Variable is not defined" << endl;
+                exit(1); 
+            }
+        }
+
+    public:
+        SymbolTableBuilder() : symbols(SymbolTable()) {}
+        void visit(node_ptr n) {
+            switch(n->get_op()) {
+                case NUM:
+                    visit_num(n);
+                    break;
+                case BIN:
+                     visit_binop(n);
+                     break;
+                case UNARY:
+                     visit_unary(n);
+                     break;
+                case COMPOUND:
+                     visit_compound(n);
+                     break;
+                case EQ:
+                     visit_assign(n);
+                     break;
+                case VAR:
+                     visit_var(n);
+                     break;
+                case NOOP:
+                    visit_noop(n);
+                    break;
+                case PROGRAM:
+                    visit_program(n);
+                    break;
+                case BLOCK:
+                    visit_block(n);
+                    break;
+                case VARDECL:
+                    visit_vardecl(n);
+                    break;
+                //case TYPE:
+                //    visit_type(n);
+                //    break;
+            }
+
+        }
+};
